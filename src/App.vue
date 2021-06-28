eb841f8351bb0a3299f37312bc10d3500ef7b0e0
T J W <gamingfreddy11@gmail.com>
2021-06-27 20:44:06 -0400

    Added function to store url locally in QRHistory

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@ export default {
   data: () => {
     return {
       currentURL: "https://github.com/code4tomorrow/javascript",
+      str: "",
     };
   },
   mounted() {},
--- src/components/QRHistory.vue
+++ src/components/QRHistory.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="history"></div>
+  <div class="history">
+    <button @click="saveUrls()">Send to History</button>
+    <input v-model="currentURL"/>
+  </div>
+
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => {
+    return {
+      urlList: JSON.parse(localStorage.getItem("URLs")) || [],
+      currentURL: "https://github.com/code4tomorrow/javascript",
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+    saveUrls() {
+      this.urlList.unshift(this.currentURL)
+      localStorage.setItem("URLs", JSON.stringify(this.urlList))
+    }
+  }
+};
+
 </script>
 
 <style>
